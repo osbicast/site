8ef83a7a890534304bb764890739f5b3556bec90
fernandommota <mota.fernandomaia@gmail.com>
2019-04-02 20:59:30 +0000

    add credits;

--- wp-content/themes/wp-bootstrap-4/footer.php
+++ wp-content/themes/wp-bootstrap-4/footer.php
@@ -11,52 +11,77 @@
 
 ?>
 
-	</div><!-- #content -->
+	</div><!-- #content -->	
 
-	<footer id="colophon" class="site-footer text-center bg-white mt-4 text-muted">
+	<div id="about_section" class="text-center bg-white mt-4 text-muted" style="margin-top: 0 !important; padding-top: 3rem;padding-bottom: 3rem;">
+		<div class="container">
+			<h3>Sobre</h3>
+			<p class="lead">
+				OSBI Cast é um podcast de difusão de conhecimento e de divulgação da comunidade de <i>Business Intelligence</i> de Código Aberto. Composto pelo quadro <i>interview</i>  com episódios mensais sempre com alguma personalidade da comunidade de BI.
+			</p>
+		</div>
+	</div>
+	
+	<footer id="colophon" class="site-footer text-center bg-white text-muted">
 
 		<section class="footer-widgets text-left">
 			<div class="container">
 				<div class="row">
-					<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-						<div class="col">
-							<aside class="widget-area footer-1-area mb-2">
-								<?php dynamic_sidebar( 'footer-1' ); ?>
-							</aside>
+					
+						<div class="col-xs-12 col-md-4">
+							<h5>Últimos episódios</h5>
+							<ul>
+								<?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+								<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+								<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+								<?php 
+								endwhile;
+								wp_reset_postdata();
+								?>
+							</ul>
 						</div>
-					<?php endif; ?>
-
-					<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-						<div class="col">
-							<aside class="widget-area footer-2-area mb-2">
-								<?php dynamic_sidebar( 'footer-2' ); ?>
-							</aside>
+					
+						<div class="col-xs-12 col-md-4">
+							<h5>Host</h5>
+							<a href="https://twitter.com/fernandommota" target="no_blank">
+								<div>
+									<div class="d-inline float-left" style="margin-right: 10px; filter: grayscale(100%);">
+										<img src="https://osbicast.com/wp-content/uploads/2019/03/PERFIL_LATTES.jpg" class="img-thumbnail" style="height: 60px;" alt="Host Fernando Maia">
+									</div>
+									<div class="d-inline float-left">
+										<span class="d-block">Fernando Maia da Mota</span>
+										<small class="d-block">BI Analyst and Pentaho Specialist</small>
+										<small class="d-block">@fernandommota</small>
+									</div>
+								</div>
+							</a>
 						</div>
-					<?php endif; ?>
-
-					<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-						<div class="col">
-							<aside class="widget-area footer-3-area mb-2">
-								<?php dynamic_sidebar( 'footer-3' ); ?>
-							</aside>
+					
+						<div class="col-xs-12 col-md-4">
+							<h5>Vitrine</h5>
+							<a href="https://www.instagram.com/designerflaviaborato/" target="no_blank">
+								<div>
+									<div class="d-inline float-left" style="margin-right: 10px; filter: grayscale(100%);">
+										<img src="https://instagram.fcgr1-1.fna.fbcdn.net/vp/1e4a54a9b159f47621b96932f7281831/5D331B68/t51.2885-19/s150x150/54447253_155373825396496_7617288812019843072_n.jpg?_nc_ht=instagram.fcgr1-1.fna.fbcdn.net" class="img-thumbnail" style="height: 60px;" alt="Host Fernando Maia">
+									</div>
+									<div class="d-inline float-left">
+										<span class="d-block">Flávia Borato</span>
+										<small class="d-block">Designer Gráfico</small>
+										<small class="d-block">@designerflaviaborato</small>
+									</div>
+								</div>
+							</a>
 						</div>
-					<?php endif; ?>
-
-					<?php if ( is_active_sidebar( 'footer-4' ) ) : ?>
-						<div class="col">
-							<aside class="widget-area footer-4-area mb-2">
-								<?php dynamic_sidebar( 'footer-4' ); ?>
-							</aside>
-						</div>
-					<?php endif; ?>
+					
 				</div>
 				<!-- /.row -->
 			</div>
 		</section>
-
+	
+		<hr>
 		<div class="container">
 			<div class="site-info">
-				<a href="<?php echo esc_url( 'osbicast.com' ); ?>"><?php echo date("Y")?> © OSBI Cast - O podcast da comunidade open source de BI</a>
+				<a href="<?php echo esc_url( 'osbicast.com' ); ?>"><?php echo date("Y")?> © OSBI Cast - O podcast da comunidade de BI open source</a>
 			</div><!-- .site-info -->
 		</div>
 		<!-- /.container -->

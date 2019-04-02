241870a6038c68121efae1dfd551dc7cd273eda6
fernandommota <mota.fernandomaia@gmail.com>
2019-04-02 13:12:22 +0000

    first commit

---
+++ wp-content/themes/wp-bootstrap-4/footer.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_4
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer text-center bg-white mt-4 text-muted">
+
+		<section class="footer-widgets text-left">
+			<div class="container">
+				<div class="row">
+					<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+						<div class="col">
+							<aside class="widget-area footer-1-area mb-2">
+								<?php dynamic_sidebar( 'footer-1' ); ?>
+							</aside>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+						<div class="col">
+							<aside class="widget-area footer-2-area mb-2">
+								<?php dynamic_sidebar( 'footer-2' ); ?>
+							</aside>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+						<div class="col">
+							<aside class="widget-area footer-3-area mb-2">
+								<?php dynamic_sidebar( 'footer-3' ); ?>
+							</aside>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'footer-4' ) ) : ?>
+						<div class="col">
+							<aside class="widget-area footer-4-area mb-2">
+								<?php dynamic_sidebar( 'footer-4' ); ?>
+							</aside>
+						</div>
+					<?php endif; ?>
+				</div>
+				<!-- /.row -->
+			</div>
+		</section>
+
+		<div class="container">
+			<div class="site-info">
+				<a href="<?php echo esc_url( 'osbicast.com' ); ?>"><?php echo date("Y")?> © OSBI Cast - O podcast da comunidade open source de BI</a>
+			</div><!-- .site-info -->
+		</div>
+		<!-- /.container -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
